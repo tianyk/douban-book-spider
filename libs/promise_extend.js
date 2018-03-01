8b63f8434c5d0b58a57ce262a0eef9330d7925b9
tyk <yongketian@gmail.com>
2018-03-01 17:05:04 +0800

    extent promise

---
+++ libs/promise_extend.js
@@ -0,0 +1,17 @@
+if (typeof Promise === 'function') {
+    Promise.prototype.done = function (onFulfilled, onRejected) {
+        return this.then(onFulfilled, onRejected)
+            .catch(function (reason) {
+                // 抛出一个全局错误
+                setTimeout(() => { throw reason }, 0);
+            });
+    };
+
+    Promise.prototype.finally = function (callback) {
+        let P = this.constructor;
+        return this.then(
+            value => P.resolve(callback()).then(() => value),
+            reason => P.resolve(callback()).then(() => { throw reason })
+        );
+    };
+}
